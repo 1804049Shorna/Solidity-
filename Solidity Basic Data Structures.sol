417e10e5f62a78451fb0515032e2675710fc8075
Shanjida Nahid Shorna <50510843+1804049Shorna@users.noreply.github.com>
2022-11-25 22:03:07 +0600

    Create Solidity Basic Data Structures.sol

---
+++ Solidity Basic Data Structures.sol
@@ -0,0 +1,93 @@
+pragma solidity >=0.4.24;
+
+contract Mycontract {
+    // string public constant value = "myvalue";
+
+    // constructor() public{
+    //     value="myvalue";
+    // }
+
+    // function get() public view returns(string memory){
+    //     return value;
+    // }
+
+    // function set(string memory _value) public{
+    //     value=_value;
+
+    //exploring different types of data type
+
+    //  bool public mybool=true;
+    //  int public myint=-1;
+    //  uint public myuint=1;
+    //  uint8 public myuint8=8;
+    //  uint256 public myy=256;
+
+
+
+    // enum in solidity
+    //    enum  State {Active, Waiting, Ready}
+    //    State public state;
+    //     constructor() public{
+    //         state=State.Waiting;
+    //     }
+
+    //     function activate() public{
+    //         state=State.Active;
+    //     }
+
+    //     function isActive() public view returns(bool)
+    //     {
+    //         return state == State.Active;
+    //     }
+
+
+   
+
+
+
+
+
+
+     // structs in solidity
+    //  Person[] public people;
+    // uint256 public peoplecount;
+    //  struct Person{
+
+    //      string _firstName;
+    //      string _lastName;
+
+    //  }
+
+    //  function addPerson (string memory _firstName, string memory _lastName) public {
+    //      people.push(Person(_firstName,_lastName));
+    //      peoplecount+=1;
+    //  }
+
+
+  
+
+    //mapping in solidity
+
+    //  Person[] public people;
+    // uint256 public peoplecount=0;
+    // mapping(uint => Person) public people;
+    //  struct Person{
+    //      uint id;
+
+    //      string _firstName;
+    //      string _lastName;
+
+    //  }
+
+    //  function addPerson (string memory _firstName, string memory _lastName) public {
+    //       peoplecount+=1;
+
+    //       people[peoplecount]=Person(peoplecount,_firstName,_lastName);
+    //     //  people.push(Person(_firstName,_lastName));
+        
+    //  }
+
+
+
+
+}
